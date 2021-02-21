3bb7d486563978577f94e3f52cbe1e6ac350e198
PersonajeTm <personajetm@gmail.com>
2021-02-20 23:00:14 -0600

    Coded Anchor tags
    Add the clickable links to all the signatures

--- Javascript/artists.js
+++ Javascript/artists.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   
     let imgLinks = ["TestImages/Signatures/1800whatever.png", "TestImages/Signatures/addyTheInstaBoy.png", "TestImages/Signatures/ampilfur.png", "TestImages/Signatures/arvlo.png", "TestImages/Signatures/BugzDrawz.png", "TestImages/Signatures/cocoaJord.png", "TestImages/Signatures/colonyBees.png", "TestImages/Signatures/demoncamile.png", "TestImages/Signatures/Doodle.png", "TestImages/Signatures/doodleNoodle.png", "TestImages/Signatures/dorkyfries.png", "TestImages/Signatures/Drawz.png", "TestImages/Signatures/evichuArt.png", "TestImages/Signatures/galchy.png", "TestImages/Signatures/hobbitCole.png", "TestImages/Signatures/paigelikesdoodles.png", "TestImages/Signatures/PinkSinsila.png", "TestImages/Signatures/posiTVT.png", "TestImages/Signatures/railyTate.png", "TestImages/Signatures/theSaylor.png", "TestImages/Signatures/tinsley.png", "TestImages/Signatures/tiredStevenArt.png", "TestImages/Signatures/yysdrmz.png"];
     
+    let instaUsers = ["_1.800.whatever_", "addy_the_instaboi","ampilfur","arvl.o","bugz_drawz","cocoa_jord","colonybees","demoncamile","doodledoobug","doodle_noodle.png","dorkyfries","drawz_0319","evichuart","gal.chy","hobbitcole","paigelikesdoodles","pink_sinsila","posi_tvt","railytate","the__saylor","tin.sl3y","tired.steven_art","yoyos.dreamz",];
+    let instagramLink = "https://www.instagram.com/";
+    
     function printitems(array, where){
         array.forEach(function(element){
             $(where).append(element);
@@ -20,13 +23,30 @@ $(document).ready(function(){
     function addImgTagstoArray(arr){
         arr.forEach(function(element, index, array){
         array[index] = addImgTags(element);
-            
-});
+        
+        });
     }
+    
+    function addAnchors(user, imgtag){      
+           return(`<a target='_blank' href=` + instagramLink + user + `>` + imgtag +`</a>`);
 
-    addImgTagstoArray(imgLinks)
-    printitems(imgLinks, "#appendee")
-    /*
-    console.log(addImgTags(imgLinks[0]));
-    */
+    }
+    
+    function addingAtags(arr, user){
+        let i = 0;
+        arr.forEach(function(element, index, array){
+        array[index] = addAnchors(user[i], element);
+        i++;
+        });
+    }
+    
+//    let dummyArray = ["a", "b", "c", "d"];
+//    addingAtags(dummyArray, instaUsers);
+//   console.log(dummyArray);
+    
+    addImgTagstoArray(imgLinks);
+    addingAtags(imgLinks, instaUsers);
+    printitems(imgLinks, "#appendee");
+
+    
 });
