f16307ed62eeb054f528b8467e2ad1609b87162f
PersonajeTm <personajetm@gmail.com>
2021-03-08 22:48:01 -0600

    FINISHED MOBILE ZINE LOGIC

--- Javascript/zine.js
+++ Javascript/zine.js
@@ -2,17 +2,67 @@ $(document).ready(function(){
     
     var width = $(window).width(); 
     var height = $(window).height(); 
-
-    if (height > width) {
-    console.log("mobile");
-        $("body").remove();
-    }
-    else {
-        
-    let position = 0;
-    let images = ["TestImages/Pages/herriochi.png", "TestImages/Pages/drawz.png", "TestImages/Pages/DoodleNbluJay.png", "TestImages/Pages/Doodle.jpeg", "TestImages/Pages/galchy.png", "TestImages/Pages/seviche.png", "TestImages/Pages/chayse.png"];
+    
+    let pageHTML = `<div>
+                    <img class="piece" src="TestImages/Pages/herriochi.png">
+                    <img class="artist" src="TestImages/Signatures/herriochi.png">
+                  </div>`;
+    let images = ["TestImages/Pages/herriochi.png", "TestImages/Pages/drawz.png", "TestImages/Pages/DoodleNbluJay.png", "TestImages/Pages/Doodle.jpeg", "TestImages/Pages/galchy.png", "TestImages/Pages/seviche.png", "TestImages/Pages/chayse.png", "TestImages/Pages/tina.png"];
     let artists = [];
-    let max = 7;
+    let max = 8;
+    
+    if ((height > width) || (height >= (width - 200))) {
+        var position = 0;
+        // CSS SHIT
+        $("body").css("background-color", "yellowgreen");
+        $(".rightPage").remove();
+        $(".leftPage").remove();
+        $("#placeholder").html(pageHTML);
+        
+        $(".container").css("background-image", "url(/TestImages/bgtest3.png)"); 
+        $(".container").css("padding", "2%");
+        $(".piece").css("max-width", "80%");
+        $(".piece").css("min-width", "75%");
+        $(".piece").css("display", "block");
+        $(".piece").css("margin", "auto");
+        /// CSS END
+        
+        function updateSite(){
+            $(".piece").attr("src", images[position]);   
+        }
+        
+
+        $("#next").click(function(){
+            if (position === max){
+                position = max;
+                }else{
+                    position++; 
+                }
+            console.log(position);
+            updateSite();
+        });
+        
+        $(".previous").click(function(){
+           
+            if (position === 0){
+                position = 0;
+            }else {
+                position--; 
+            }
+            console.log(position);
+            updateSite();
+        });
+        
+        
+        
+        
+        
+        
+        
+    }/*
+    else {
+           let position = 0;
+    
     
     $("#next").click(function(){
  // CHANGE IF MAX IS AN EVEN NUMBER
@@ -51,6 +101,6 @@ $(document).ready(function(){
     
         
     
-    
+    */
     
 });
