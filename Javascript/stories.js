0f4610d2345d18471e19da4b9d3c6f55d3f58fa1
PersonajeTm <personajetm@gmail.com>
2021-03-09 22:51:56 -0600

    start of stories

---
+++ Javascript/stories.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    
+    const story1 = "a";
+    const story2 = "b";
+    const story3 = "c";
+    let stories = [story1, story2, story3];
+    
+    
+let e;
+    
+  $("body").click(function(){
+      e = $('[name=options]').val();
+      console.log(e);
+  });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
