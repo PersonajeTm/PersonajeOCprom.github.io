6c14bd1599c151f53cc543e767f49ed2939e8bad
PersonajeTm <personajetm@gmail.com>
2021-03-10 01:23:02 -0600

    finished stories!!

--- Javascript/stories.js
+++ Javascript/stories.js
@@ -1,25 +1,122 @@
 $(document).ready(function(){
     
-    const story1 = "a";
+    const story1 = `<p dir="ltr" style="line-height:2.4;margin-top:0pt;margin-bottom:0pt;">&nbsp; &nbsp; It&rsquo;s already eleven o&rsquo;clock. The grey clouds float and fade away into the darkness of the night and a steady breeze continues to chill the atmosphere. Fewer and fewer cars pass as time goes on. I trace my hand over the same spot on the concrete. His voice replays in my head.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Yeah, I&rsquo;ll be there,&rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Don&rsquo;t worry baby, I won&rsquo;t let you down,&rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;I promise you won&rsquo;t be alone this time,&rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">Okay, but where are you? I check my phone again to see that only a few minutes have passed. &ldquo;You said you&rsquo;d be here by 9 Mason!&rdquo; I sent at nine fifteen. &ldquo;I know, I&rsquo;m already on my way,&rdquo; he replies at nine twenty five. &ldquo;Any closer?&rdquo; nine thirty. &ldquo;Hello?&rdquo; nine forty.&nbsp;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;You said you&rsquo;d be here this time...&rdquo; ten o&rsquo;clock.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">What&rsquo;s the point? I bury my head in my knees. He&rsquo;s not coming. I don&rsquo;t know why I thought this time would be any different.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Arya?&rdquo; a familiar voice echoes in the distance.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">My head pops up to see a vaguely familiar figure approaching from the shadows, the click of their heels getting closer. I couldn&rsquo;t recognize who it was until they stepped out into the faint light of the street lamp. I don&rsquo;t know how I didn&apos;t notice before with her tall, thin build and long untamed curls. She wore a red two-piece along with a noodle strap sparkly top and a knee-length skirt, ruffles all the way down.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Lou?&rdquo; I look up to meet her gaze. She doesn&rsquo;t seem to have the worried expression I thought she would. All I see is anger and irritation. &ldquo;You look amazing,&rdquo; I force a smile.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Thanks and.. So do you,&rdquo; She leans on the pole of the street light. Seconds silently pass &nbsp;until a sigh finally escapes her lips. &ldquo;He&rsquo;s a piece of shit, you know that right?&rdquo; I bury my head deeper into my gut. &ldquo;I don&rsquo;t even know why he gets your hopes up if he knows he&rsquo;s not gonna come. You don&rsquo;t deserve this...&rdquo; Tears sting at my eyes and a tightness rises in my chest. The clink of her footsteps gets louder until I can feel her presence next to me. The sting &nbsp;worsens until I can&rsquo;t force back my tears anymore. In the midst of the chilly night, I feel an abundance of warmth around me.&nbsp;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;I feel like he doesn&rsquo;t even care...&rdquo; I force through the sobs. She doesn&rsquo;t say anything, just continues to comfort me for as long as I need. After a while I wipe my face and smile.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Feel any better?&rdquo; she asks. I nod.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Thanks,&rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;No problem, I just wish that he could&rsquo;ve tried to be better, especially for you... &rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">Suddenly, we both hear footsteps in the distance. Both of our attentions linger on the other side of the street, where a hooded figure makes their way toward us.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;It couldn&rsquo;t be...&rdquo; I whisper.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;It isn&rsquo;t,&rdquo; Lou stands up.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;ARYA!&rdquo; the figure shouts. It&rsquo;s another feminine voice, one that I could recognize &nbsp;anywhere.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;AVE!&rdquo; I shouted back. We ran to hug each other, &ldquo;What are you doing here? I thought school events like this weren&rsquo;t your style,&rdquo; I smirked.</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;They weren&rsquo;t, and they still aren&rsquo;t. But I know how you are, and I couldn&rsquo;t just let you sit here at night waiting for that asshole.&rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">I frowned, &ldquo;So you apparently know the future too,&rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Yup! And in the near future, I see us at my house eating junk food and watching horror movies with the lights off,&rdquo; Funny how a few seconds ago I was crying my eyes out, but now I couldn&apos;t wipe the grin off my face even if I tried. She always knows exactly what I want to hear, and exactly how to cheer me up. &ldquo;So...&rdquo; she starts again, &ldquo;Are you down?&rdquo;</p>
+<p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">Tap. Tap. Tap.&nbsp;The footsteps start up again. &ldquo;Yeah, of course!&rdquo;&nbsp;Tap. Tap. Tap.&nbsp;I catch Lou right before she goes in the door and wave her goodbye, right as we start to get going, my phone buzzes. I know who it is, but I&rsquo;m not answering it.&nbsp;Bzzt! Bzzt! Bzzt!&nbsp;It vibrates in my hand.&nbsp;</p>
+        <p dir="ltr" style="line-height:2.4;text-indent: 36pt;margin-top:0pt;margin-bottom:0pt;">&ldquo;Are you gonna answer?&rdquo; Ave asks. I look at my phone and stare at the contact photo. I remember the day we took it. We look so happy. Oh, how naive I was to think that he&rsquo;d always stick by my side. He can&rsquo;t even come to a damn dance. I silence my phone before catching up with Ave. If he&rsquo;s gonna waste my time, he shouldn&rsquo;t mind me not wanting to answer.</p>`;
     const story2 = "b";
     const story3 = "c";
     let stories = [story1, story2, story3];
+    let titles =["Not a Let Down", "title2", "title3"];
+    let authors = ["TestImages/Signatures/yysdrmz.png", "TestImages/Signatures/ampilfur.png", "TestImages/Signatures/arvlo.png", "TestImages/Signatures/BugzDrawz.png"]
+    var selected;
+    var clicks = 0;
+    var size = 16;
+    
+    $("#dropdown").change(function(){
+        selected = $("#dropdown option:selected").val();
+        
+        updateStory(selected);
+//        if (selected === "select"){
+//            console.log("select");
+//            } else if (selected === "1"){
+//                $("#holder").html(stories[0]);
+//            }else if (selected === "2"){
+//                console.log("2");
+//            }else if (selected === "3"){
+//                console.log("3");
+//            }
+    });
+    
+    function updateStory(value){
+        $("#holder").html(stories[value]);
+        $("#storyTitle").text(titles[value]);
+        $("#author").attr("src", authors[value]);
+    }
+    
+    const red = "#6e0a14";
+    const white = "antiquewhite";
+    const black = "black";
+
+    //CSS
+    $("#1").css("background-color", "antiquewhite"); $("#1").css("border", "solid 2px #6e0a14"); $("#2").css("background-color", "#6e0a14"); $("#2").css("border", "solid 2px grey"); $("#3").css("background-color", "black"); $("#3").css("border", "solid 2px antiquewhite");
+    
+    function changeTheme(bg, text){
+        $("body").css("background-color", bg);
+        $("#holder").css("color", text);
+        $("#storyTitle").css("color", text);
+    }
     
     
-let e;
-    
-  $("body").click(function(){
-      e = $('[name=options]').val();
-      console.log(e);
-  });
+    $("#1").click(function(){
+        changeTheme(white, black);
+    });
     
     
+     $("#2").click(function(){
+         clicks++;
+         if (clicks === 2){
+             changeTheme(red, black);
+             clicks = 0;
+             }else {
+                 changeTheme(red, white);
+             }
+        
+    });
     
     
+     $("#3").click(function(){
+        changeTheme(black, white);
+    });
+    
+    function changeSize(interval){
+        let numero = size + interval;
+        size = numero;
+        $("#holder").css("font-size", numero);
+    }
     
     
+    $("#increase").click(function(){
+        
+        if (size >= 30){
+            // NOTHING LMAO
+        }else {
+            changeSize(2);
+        }
+                    console.log(size);
+
+    });
     
-    
+    $("#decrease").click(function(){
+        if (size === 16){
+            // NOTHING LMAO
+        }else {
+            changeSize(-2);
+        }
+        
+
+    });
     
     
     
